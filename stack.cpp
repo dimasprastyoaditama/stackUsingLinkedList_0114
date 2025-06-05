6933006ce83ab6af9441190b6e0319afdcc93d40
dimasprastyoaditama <prastyod647@gmail.com>
2025-06-05 15:14:13 +0700

    Menambahkan implementasi kelas Node dan konfigurasi

--- stack.cpp
+++ stack.cpp
@@ -1,23 +1,27 @@
 #include <iostream>
-using namespace std;
+using namespace std;    
 
-class Node
+//Node class representing a single node in the linked list
+class Node 
 {
 public:
     int data;
-    Node *next;
+    Node* next;
 
     Node()
     {
-        next = Null;
+        next = NULL;
     }
 };
 
-// Stack class
-class Stack
+//stack class
+class Stack 
 {
-private:
-    Node *top; // Pointer to the top of the stack
+    private:
+        Node* top; // Pointer to the top node of the stack
+    public:
+        Stack() 
+        {
+            top = NULL; // Initialize the stack with a null top pointer
+        }
 
-
-};
