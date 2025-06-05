bef6e362f2059980d87f62b3d2062a43e27b2e93
dimasprastyoaditama <prastyod647@gmail.com>
2025-06-05 15:29:13 +0700

    Menambahkan penanganan untuk pilihan tidak valid dalam metode main pada kelas Stack

--- stack.cpp
+++ stack.cpp
@@ -104,3 +104,6 @@ class Stack
             case 4:
                 cout << "Exiting program." << endl;
                 break;
+            default:
+                cout << "Invalid choice. Try again." << endl;
+                break;
